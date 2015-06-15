c5732c00c07766f0c3a10bd72a64c3a0c40ab04c
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2015-06-15 10:45:19 +0100

    Replaced Panopto with review

--- lang/en/block_panopto.php
+++ lang/en/block_panopto.php
@@ -16,15 +16,15 @@
 
 /**
  * @package block_panopto
- * @copyright  Panopto 2009 - 2015 with contributions from Spenser Jones (sjones@ambrose.edu)
+ * @copyright  Re:View (Panopto) 2009 - 2015 with contributions from Spenser Jones (sjones@ambrose.edu)
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Panopto';
-$string['panopto:addinstance'] = 'Add a new Panopto block';
-$string['panopto:myaddinstance'] = 'Add a new Panopto block to my page';
+$string['pluginname'] = 'Re:View (Panopto)';
+$string['panopto:addinstance'] = 'Add a new Re:View (Panopto) block';
+$string['panopto:myaddinstance'] = 'Add a new Re:View (Panopto) block to my page';
 $string['panopto:provision_course'] = 'Provision a course';
 $string['panopto:provision_multiple'] = 'Provision multiple courses at once';
 $string['panopto:provision_asteacher'] = 'Provision as a teacher';
@@ -35,18 +35,18 @@ $string['provisioncourseselect_help'] = 'Multiple selections are possible by Ctr
 $string['unconfigured'] = 'Global configuration incomplete. Please contact your system administrator.';
 $string['unprovisioned'] = 'This course has not yet been provisioned.';
 $string['block_edit_error'] = 'Cannot configure block instance: ' . $string['unconfigured'];
-$string['block_edit_header'] = 'Select the Panopto course to display in this block.';
-$string['add_to_panopto'] = 'Add this course to Panopto (re-add to sync user lists)';
+$string['block_edit_header'] = 'Select the Re:View (Panopto) course to display in this block.';
+$string['add_to_panopto'] = 'Add this course to Re:View (Panopto) (re-add to sync user lists)';
 $string['or'] = 'OR';
 $string['existing_course'] = 'Select an existing course:';
 $string['block_global_instance_name'] = 'Moodle Instance Name';
-$string['block_global_instance_description'] = 'This value is prefixed before usernames and course-names in Panopto.';
-$string['block_global_hostname'] = 'Panopto Server Hostname';
+$string['block_global_instance_description'] = 'This value is prefixed before usernames and course-names in Re:View (Panopto).';
+$string['block_global_hostname'] = 'Re:View (Panopto) Server Hostname';
 $string['block_global_application_key'] = 'Application Key';
-$string['block_global_add_courses'] = 'Add Moodle courses to Panopto';
+$string['block_global_add_courses'] = 'Add Moodle courses to Re:View (Panopto)';
 $string['course'] = 'Course';
-$string['no_course_selected'] = 'No Panopto course selected';
-$string['error_retrieving'] = 'Error retrieving Panopto course.';
+$string['no_course_selected'] = 'No Re:View (Panopto) course selected';
+$string['error_retrieving'] = 'Error retrieving Re:View (Panopto) course.';
 $string['live_sessions'] = 'Live Sessions';
 $string['no_live_sessions'] = 'No Live Sessions';
 $string['take_notes'] = 'Take Notes';
@@ -62,8 +62,8 @@ $string['course_settings'] = 'Course Settings';
 $string['download_recorder'] = 'Download Recorder';
 $string['show_all'] = 'Show All';
 $string['show_less'] = 'Show Less';
-$string['role_map_header'] = 'Change Panopto Role Mappings';
-$string['role_map_info_text'] = "Choose which Panopto roles a user's Moodle role will map to. <br> Unmapped roles will be given the 'Viewer' role in Panopto.
+$string['role_map_header'] = 'Change Re:View (Panopto) Role Mappings';
+$string['role_map_info_text'] = "Choose which Re:View (Panopto) roles a user's Moodle role will map to. <br> Unmapped roles will be given the 'Viewer' role in Re:View (Panopto).
  <br><br> ";
 $string['block_panopto_async_tasks'] = 'Asynchronous enrolment sync';
 
